bb6b760188d093ad43b388efb93abeb9e9b6635e
caedwil <daniel.w.a.stevenson@gmail.com>
2019-03-26 20:53:05 +1100

    Update random.h
    Added template for randomisation engine type and cleaned up usage of the class.

--- random.h
+++ random.h
@@ -8,62 +8,57 @@
 
 #include <random>
 
+template <class Engine = std::mt19937>
 class Random {
 public:
+	// Initialise the randomisation engine using an instance of random
+	// device.
+	Random() {
+		engine = Engine(std::random_device()());
+	}
+
 	// Generates a pseudo-random integer in the range [min, max].
-	static int get(int min, int max) {
+	int operator()(int min, int max) {
 		return generate<std::uniform_int_distribution, int>(min, max);
 	}
 
 	// Generates a pseudo-random unsigned integer in the range [min, max].
-	static unsigned int get(unsigned int min, unsigned int max) {
+	unsigned int operator()(unsigned int min, unsigned int max) {
 		return generate<std::uniform_int_distribution, unsigned int>(min, max);
 	}
 
-	// Generates a pseudo-random double in the range [min, max).
-	static double get(double min, double max) {
-		return generate<std::uniform_real_distribution, double>(min, max);
-	}
-
 	// Generates a pseudo-random float in the range [min, max).
-	static float get(float min, float max) {
+	float operator()(float min, float max) {
 		return generate<std::uniform_real_distribution, float>(min, max);
 	}
+
+	// Generates a pseudo-random double in the range [min, max).
+	double operator()(double min, double max) {
+		return generate<std::uniform_real_distribution, double>(min, max);
+	}
 private:
-	// Used to seed the random number generator.
-	static std::random_device device;
+	// The randomisation engine.
+	Engine engine;
 
-	// The random number generator. In this case, it is based on the 
-	// Mersenne Twister implementation.
-	static std::mt19937 rng;
-
-	// Generates a random number using the specified distribution type
+	// Generate a random number using the specified distribution type
 	// and the specified minimum and maximum values.
 	template <template<typename> class Distribution, typename T>
-	static T generate(T min, T max) {
+	T generate(T min, T max) {
 		// Declare the distribution as static so that it can be reused
-		// by subsequent calls to this function if the minimum or
+		// by subsequent calls to this function if the minimum and 
 		// maximum values have not changed.
 		static Distribution<T> distribution;
 
 		// If the minimum or maximum values have changed, generate a
-		// new distribution. Otherwise leave the current distribution
+		// new distribution. Otherwise, leave the current distribution
 		// unchanged.
 		if (distribution.min() != min || distribution.max() != max) {
 			distribution = Distribution<T>(min, max);
 		}
 
 		// Generate a random number using the current distribution.
-		return distribution(rng);
+		return distribution(engine);
 	}
-
-	// Declaring the constructor as 'private' to prevent instantiation.
-	Random() {}
 };
-
-// Define the random device and, specifically, the random number
-// generator so that they can be used.
-std::random_device Random::device;
-std::mt19937 Random::rng(Random::device());
 
 #endif
